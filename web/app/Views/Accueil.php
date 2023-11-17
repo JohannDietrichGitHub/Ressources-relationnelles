a4eb3d9e6a8651867b65ef4db3ccb269056738a5
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2023-11-17 14:53:45 +0100

    Ajout Framework CSS Bulma
    Ajout du Framework bulma + Ajout Police d'écriture Marianne

---
+++ web/app/Views/Accueil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/bulma.css" rel="stylesheet">
+    <link href="../../public/css/bulma.css" rel="stylesheet">
+    <link href="./css/custom.css" rel="stylesheet">
+    <link href="../../public/css/custom.css" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body>
+    <p class="">Test</p>
+</body>
+</html>
