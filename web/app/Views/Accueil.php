13adffc18480cc173b4207fdeed39e988750d5a2
VSanzovo <victor.sanzovo@viacesi.fr>
2023-11-18 09:50:09 +0100

    Maquette page d'accueil

--- web/app/Views/Accueil.php
+++ web/app/Views/Accueil.php
@@ -7,9 +7,73 @@
     <link href="../../public/css/bulma.css" rel="stylesheet">
     <link href="./css/custom.css" rel="stylesheet">
     <link href="../../public/css/custom.css" rel="stylesheet">
-    <title>Accueil</title>
+    <title>Ressources Relationnelles : Accueil </title>
 </head>
+<header>
+    <img>
+
+    <section>
+        <input type="button" value="Accueil">
+        <input type="button" value="Ressources">
+        <input type="button" value="FAQ">
+    </section>
+    
+    <input id="search" type="text">
+
+    <section>
+        <input type="button" value="Inscription">
+        <input type="button" value="Connexion">
+    </section>
+</header>
 <body>
-    <p class="">Test</p>
+    <section>
+        <h1>(RE)Sources Relationnelles</h1>
+        <h3>L'application pour améliorer vos relations</h3>
+    </section>
+
+    <section>
+        <h2>Ressources Populaires</h2>
+        <ul>
+    		<li>Ressource 1
+    		<li>Ressource 2
+    		<li>Ressource 3
+    		<li>Ressource 4
+    		<li>Ressource 5
+		</ul>
+        <input type="button" value="Découvrir...">
+    </section>
+
+    <section>
+        <h2>Informations pratiques</h2>
+        <ul>
+    		<li>-Info
+    		<li>-Info
+            <li>-Info
+            <li>-Info
+            <li>-Info
+            <li>-Info
+            <li>-Info
+            <li>-Info
+		</ul>
+        <input type="button" value="Plus d'informations">
+    </section>
 </body>
+<footer>
+    <img>
+
+    <section>
+        <img>
+        <img>
+        <img>
+    </section>
+    
+    <section>
+        <ul>
+    	    <li> Plan du site
+            <li> Contact
+            <li> Mentions légales
+            <li> Gestion des données personelles    
+	    </ul>
+    </section>
+</footer>
 </html>
