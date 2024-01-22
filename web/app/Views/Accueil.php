1f09ef4bb8e76654bf5f762da3938ba1556ba220
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-01-22 13:49:50 +0100

    Creation page d'accueil

--- web/app/Views/Accueil.php
+++ web/app/Views/Accueil.php
@@ -1,79 +1,202 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/bulma.css" rel="stylesheet">
-    <link href="../../public/css/bulma.css" rel="stylesheet">
-    <link href="./css/custom.css" rel="stylesheet">
-    <link href="../../public/css/custom.css" rel="stylesheet">
-    <title>Ressources Relationnelles : Accueil </title>
+    <title>Food Seeker Accueil</title>
+    <!-- STYLES -->
+    <style {csp-style-nonce}>
+
+.bg-accueil {
+overflow: hidden;
+position: relative;
+}
+
+.bg-accueil::before {
+content: "";
+position: absolute;
+left: 0;
+right: 0;
+z-index: -1;
+
+display: block;
+background-image: url("./images/bg-title.jpg");
+background-size:cover;
+overflow: hidden;
+background-position-y: -0px;
+width: 110%;
+height: 110%;
+left: -5%;
+top: -5%;
+
+
+filter: blur(12px);
+}
+</style>
 </head>
-<header>
-    <img>
+<body class= "bg-body-tertiary">
 
-    <section>
-        <input type="button" value="Accueil">
-        <input type="button" value="Ressources">
-        <input type="button" value="FAQ">
-    </section>
-    
-    <input id="search" type="text">
+<main>
 
-    <section>
-        <input type="button" value="Inscription">
-        <input type="button" value="Connexion">
-    </section>
-</header>
-<body>
-    <section>
-        <h1>(RE)Sources Relationnelles</h1>
-        <h3>L'application pour améliorer vos relations</h3>
-    </section>
 
-    <section>
-        <h2>Ressources Populaires</h2>
-        <ul>
-    		<li>Ressource 1
-    		<li>Ressource 2
-    		<li>Ressource 3
-    		<li>Ressource 4
-    		<li>Ressource 5
-		</ul>
-        <input type="button" value="Découvrir...">
-    </section>
+  <section class="my-3 py-5 text-center container bg-accueil">
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light text-white">Food Seeker</h1>
+        <p class="lead text-white">Partagez et trouvez vos idées de recettes sur Food Seeker.</p>
+        <p>
+          <a href="#" class="btn btn-primary my-2">Main call to action</a>
+          <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+        </p>
+      </div>
+    </div>
+  </section>
 
-    <section>
-        <h2>Informations pratiques</h2>
-        <ul>
-    		<li>-Info
-    		<li>-Info
-            <li>-Info
-            <li>-Info
-            <li>-Info
-            <li>-Info
-            <li>-Info
-            <li>-Info
-		</ul>
-        <input type="button" value="Plus d'informations">
-    </section>
+  <div class="album py-5 bg-body-tertiary">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-body-secondary">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</main>
+
 </body>
-<footer>
-    <img>
-
-    <section>
-        <img>
-        <img>
-        <img>
-    </section>
-    
-    <section>
-        <ul>
-    	    <li> Plan du site
-            <li> Contact
-            <li> Mentions légales
-            <li> Gestion des données personelles    
-	    </ul>
-    </section>
-</footer>
 </html>
--- web/app/Views/header.php
+++ web/app/Views/header.php
@@ -1,33 +1,43 @@
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link href="./css/css.css" rel="stylesheet">  
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <link href="./css/custom.css" rel="stylesheet">  
+  <script src="./bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </head>
 <body>
-<nav class="navbar navbar-expand-lg green-foodseeker">
+<nav class="navbar navbar-dark navbar-expand-lg custom-dark-blue">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?= base_url('/'); ?>">Food Seeker</a>
+
+    <a class="navbar-brand" href="#">
+      <img src="./media/banner.png" href="<?= base_url('/'); ?>" alt="" height="60">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?= base_url('/'); ?>">Accueil</a>
+          <a class="nav-link active text-light" aria-current="page" href="<?= base_url('/'); ?>">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?= base_url('/recette') ?>">Recettes</a>
+          <a class="nav-link text-light" href="<?= base_url('/ressources') ?>">Ressources</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="<?= base_url('/FAQ') ?>">FAQ</a>
         </li>
       </ul>
-      <form class="d-flex" role="search" action="<?= base_url('/recherche') ?>" method="get">
-        <input class="form-control me-2" type="text" name=rechercheRecette placeholder="Recette" aria-label="Search">
-        <button class="btn btn-outline-dark" type="submit">Rechercher</button>
-      </form>
-      <div class="d-flex ms-5 me-2">
-        <button class="btn btn-light" type="submit" onclick="window.location.href='<?= base_url('login'); ?>'">Espace Client</button>
+      
+      <div class="d-flex flex-column ms-5-lg me-2">
+        <button class="btn btn-secondary my-lg-1 custom-button" type="submit" onclick="window.location.href='<?= base_url('inscription'); ?>'">Inscription</button>
+        <button class="btn btn-primary my-4 my-lg-1 custom-button" type="submit" onclick="window.location.href='<?= base_url('connexion'); ?>'">Connexion</button>
       </div>
+      
+      <form class="d-flex search-button align-items-center" role="search" action="<?= base_url('/recherche') ?>" method="get">
+        <input class="form-control shadow-none border-0" type="text" name=recherche placeholder="Rechercher..." aria-label="Search">
+        <object class="align-middle"data="./icon/search.svg" height="30"> </object>
+      </form>
     </div>
     <!-- [TODO] 05/01/2024 - Changer cette partie pour afficher les messages plus proprement -->
     <?php if (session()->has('success')) : ?>
