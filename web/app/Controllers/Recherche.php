e71d036f3095f8d6939a64f38707bb434c275bbd
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2024-04-05 15:08:48 +0200

    fix réparation 3

--- web/app/Controllers/Recherche.php
+++ web/app/Controllers/Recherche.php
@@ -10,66 +10,54 @@ use App\Controllers\Ressource;
 
 $session = \Config\Services::session();
 
+//Permet d'effectuer la recherche de ressources dans la BDD
 class Recherche extends Controller
 {
-
+    //Fonction principale
     public function index()
     {
-        try {
-            //Gestion de la requête pour récupérer la zone nom du lien
-            $request = \Config\Services::request();
-            $request = service('request');
-            $nomRecherche = $request->getVar('nom');
+        //Gestion de la requête pour récupérer la zone nom du lien
+        $request = \Config\Services::request();
+        $request = service('request');
+        $nomRecherche = $request->getVar('nom');
 
-            //Recherche SQL dans la BDD
-            $resultatsID = $this->rechercherSQL($nomRecherche);
+        //Recherche SQL dans la BDD
+        $resultatsID = $this->rechercherSQL($nomRecherche);
+        //initialisation du tableau de résultats
 
-            //initialisation du tableau
-            $resultats = [];
+        $resultats = [];
 
-            foreach ($resultatsID as $id) {
-                $ressourceModel = new M_Ressource();
+        //Rechercher chaque ressource en fonction du champs de recherche effectué
+        foreach ($resultatsID as $id) {
+            $ressourceModel = new M_Ressource();
 
-                // Recherchez la recette par ID
-                $ressource = $ressourceModel->find($id->res_id);
-                $ressource->categorie = Ressource::recupCategorieRessource($ressource->RES_ID);
-                $ressource->type = Ressource::recupTypeRessource($ressource->RES_ID);
-                $ressource->relations = Ressource::recupRelationsRessource($ressource->RES_ID);
+            // Recherchez la recette par ID
+            $ressource = $ressourceModel->find($id->res_id);
+            $ressource->categorie = Ressource::recupCategorieRessource($ressource->RES_ID);
+            $ressource->type = Ressource::recupTypeRessource($ressource->RES_ID);
+            $ressource->relations = Ressource::recupRelationsRessource($ressource->RES_ID);
 
-                $resultats[] = $ressource;
-            }
-
-            $data = [
-                'resultats' => $resultats
-            ];
-
-            $content = view('scr_Recherche', $data);
-            
-            return $content;
-        } catch (\Exception $e) {
-            log_message('error', $e->getMessage());
-            // Afficher une page d'erreur ou rediriger vers une page d'erreur
-            // return view('error_page', ['message' => 'Une erreur s\'est produite.']);
-            return 'Une erreur s\'est produite.';
+            $resultats[] = $ressource;
         }
+        //renvoi de la vue recherche et des résultats trouvés.
+        $data = [
+            'resultats' => $resultats
+        ];
+        $content = view('scr_Recherche', $data);   
+        return $content;
     }
 
+    //Fonction permettant d'effectuer une requête SQL en fonction d'un nom équivalent
     private function rechercherSQL($_nomRecherche)
     {
-        try {
-            $_nomRecherche = strval($_nomRecherche);
-            $ressourceModel = new M_Ressource();
-            $resultats = $ressourceModel->select('res_id')
-                ->like('res_nom', $_nomRecherche)
-                ->where('RES_ETAT', 'A')
-                ->where('RES_VALIDE', 'O')
-                ->findAll();
+        $_nomRecherche = strval($_nomRecherche);
+        $ressourceModel = new M_Ressource();
+        $resultats = $ressourceModel->select('res_id')
+            ->like('res_nom', $_nomRecherche)
+            ->where('RES_ETAT', 'A')
+            ->where('RES_VALIDE', 'O')
+            ->findAll();
 
-            return $resultats;
-        } catch (\Exception $e) {
-            log_message('error', $e->getMessage());
-            return []; // Retourne un tableau vide en cas d'erreur
-        }
+        return $resultats;
     }
-
 }
