1252c81b5bcbb207ddaa1f8c595af5cc6184f378
Florianmath <florian.mathis37@gmail.com>
2024-05-28 14:14:11 +0200

    Finalisation des tests unitaires - Commentaire

--- web/tests/unit/commentaire/MyCommentaireShould.php
+++ web/tests/unit/commentaire/MyCommentaireShould.php
@@ -2,10 +2,12 @@
 
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\Test\CIUnitTestCase;
+use App\Models\M_Commentaire;
 
 class MyCommentaireShould extends CIUnitTestCase
 {
-    public function testCommentaireNonExistant()
+    
+    public function CommentaireNonExistant()
     {
         // Créer une instance contrôleur
         $controller = new \App\Controllers\Commentaire();
@@ -19,6 +21,36 @@ class MyCommentaireShould extends CIUnitTestCase
         echo 'Résultat : ';
         var_dump($result);
     }
+
+    // Test unitaire
+    public function testAjouterCommentaire()
+    {
+        $_POST['commentaire_contenu'] = 'Test commentaire contenu';
+        $_POST['commentaire_uti_id'] = 3; // ID de l'utilisateur
+        $_POST['commentaire_res_id'] = 1; // ID de la ressource
+    
+        // Créez un double de votre modèle de commentaire
+        $commentaireModelMock = $this->getMockBuilder(M_Commentaire::class)
+                                     ->onlyMethods(['insert'])
+                                     ->getMock();
+    
+        // Définissez le comportement attendu du double
+        $commentaireModelMock->expects($this->once())
+                             ->method('insert')
+                             ->with(['COM_CONTENU' => 'Test commentaire contenue', 'COM_UTI_ID' => 3, 'COM_RES_ID' => 1, 'COM_TSP_CRE' => '2024-05-05', 'COM_VISIBILITE' => 'A'])
+                             ->willReturn(true); // Simule l'insertion réussie
+
+        // Injecter le double dans le contrôleur
+        $controller = new \App\Controllers\Commentaire($commentaireModelMock);
+
+        // Appeler la méthode à tester
+        $result = $controller->ajouterCommentaire();
+        dd($result);
+        // Vérifier le comportement
+        $this->assertTrue($result); // Vérifier si l'ajout s'est effectué avec succès
+    }
+
+
 
     public function DeleteCommentaireId1()
     {
